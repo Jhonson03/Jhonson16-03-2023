c213cccbfb5346e60f4a852f2e2c0993be34bc9d
Jhonson03 <ls0303032022@unab.edu.sv>
2023-03-16 22:41:56 -0600

    Add Project

---
+++ Project/Program.cs
@@ -0,0 +1,30 @@
+﻿using Project.Models;
+
+using AlmacenContext db = new AlmacenContext();
+
+Producto producto = new Producto();
+
+Console.Write("Ingrese el nombre del producto -> ");
+producto.Nombre = Console.ReadLine();
+
+Console.Write("Ingrese una breve descripcion del producto -> ");
+producto.Descripcion = Console.ReadLine();
+
+Console.Write("Ingrese el precio del producto -> $");
+producto.Precio = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Ingrese la cantidad de productos existentes -> ");
+producto.Stock = Convert.ToInt32(Console.ReadLine());
+
+db.Productos.Add(producto);
+db.SaveChanges();
+
+var ListProductos = db.Productos.ToList();
+
+foreach (var i in ListProductos)
+{
+    Console.WriteLine($"\nNombre: {i.Nombre}");
+    Console.WriteLine($"Descripcion: {i.Descripcion}");
+    Console.WriteLine($"Precio: ${i.Precio}");
+    Console.WriteLine($"Stock {i.Stock}");
+}
